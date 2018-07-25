2115e72174e7211f71bca1766c787b8c1b541449
austinbrown34 <austinbrown34@gmail.com>
2018-07-25 12:49:33 -0600

    Csv parsing (#1)
    * fix: add __pycache__ to gitignore
    
    * feat: Add store_locations.csv to repo
    
    * feat: Provide StoresParser to interface parsing of store data csvs
    
    * test: Add basic test coverage for StoresParser

---
+++ csv_parser.py
@@ -0,0 +1,49 @@
+import csv
+import codecs
+
+
+class StoresParser(object):
+
+    def __init__(self, file_path, encoding='utf-8-sig', delimiter='\t'):
+        self.file_path = file_path
+        self.encoding = encoding
+        self.delimiter = delimiter
+        self.dict = None
+
+    @property
+    def file_path(self):
+        return self.__file_path
+
+    @file_path.setter
+    def file_path(self, fp):
+        self.__file_path = fp
+
+    @property
+    def encoding(self):
+        return self.__encoding
+
+    @encoding.setter
+    def encoding(self, enc):
+        self.__encoding = enc
+
+    @property
+    def delimiter(self):
+        return self.__delimiter
+
+    @delimiter.setter
+    def delimiter(self, delim):
+        self.__delimiter = delim
+
+    @property
+    def dict(self):
+        return self.__dict
+
+    @dict.setter
+    def dict(self, dic):
+        self.__dict = dic
+
+    def get_dict(self):
+        with codecs.open(self.file_path, 'r', encoding=self.encoding) as f:
+            reader = csv.DictReader(f, delimiter=self.delimiter)
+            self.dict = [r for r in reader]
+        return self.dict
