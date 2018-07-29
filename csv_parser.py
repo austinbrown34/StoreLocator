8f28664aea1a55946f86c982962f7b16bf2fd0ee
Austin <austinbrown34@gmail.com>
2018-07-29 05:07:07 -0600

    docs: Add documentation for StoresParser and its tests

--- csv_parser.py
+++ csv_parser.py
@@ -1,9 +1,15 @@
-import csv
 import codecs
-from constants import DEFAULT_ENCODING, DEFAULT_DELIMITER
+from constants import (
+    DEFAULT_DELIMITER,
+    DEFAULT_ENCODING
+)
+import csv
 
 
 class StoresParser(object):
+    """StoresParser converts a CSV of stores data into a list of stores.
+
+    """
 
     def __init__(
             self,
@@ -11,6 +17,19 @@ class StoresParser(object):
             encoding=DEFAULT_ENCODING,
             delimiter=DEFAULT_DELIMITER
             ):
+        """Initialization creates a StoresParser instance to parse provided CSV.
+
+        StoresParser is initialized with a relative file path to a CSV
+        containing data about stores.  Optionally, the encoding of the CSV
+        and the field delimiter may be passed to dictate how the CSV will
+        be parsed.
+
+        Args:
+            file_path (str): Relative path to the CSV.
+            encoding (str, optional): Encoding of the CSV.
+            delimiter (str, optional): Field delimiter of the CSV.
+
+        """
         self.file_path = file_path
         self.encoding = encoding
         self.delimiter = delimiter
@@ -18,37 +37,73 @@ class StoresParser(object):
 
     @property
     def file_path(self):
+        """Getter for file_path.
+
+        """
+
         return self.__file_path
 
     @file_path.setter
     def file_path(self, fp):
+        """Setter for file_path.
+
+        """
+
         self.__file_path = fp
 
     @property
     def encoding(self):
+        """Getter for encoding.
+
+        """
+
         return self.__encoding
 
     @encoding.setter
     def encoding(self, enc):
+        """Setter for encoding.
+
+        """
+
         self.__encoding = enc
 
     @property
     def delimiter(self):
+        """Getter for delimiter.
+
+        """
+
         return self.__delimiter
 
     @delimiter.setter
     def delimiter(self, delim):
+        """Setter for delimiter.
+
+        """
+
         self.__delimiter = delim
 
     @property
     def dict(self):
+        """Getter for dict.
+
+        """
+
         return self.__dict
 
     @dict.setter
     def dict(self, dic):
+        """Setter for dict.
+
+        """
+
         self.__dict = dic
 
     def get_dict(self):
+        """Parses stores data from CSV and returns a list of stores (dicts).
+
+        """
+
         with codecs.open(self.file_path, 'r', encoding=self.encoding) as f:
             reader = csv.DictReader(f, delimiter=self.delimiter)
             self.dict = list(reader)
--- tests/test_StoresParser.py
+++ tests/test_StoresParser.py
@@ -1,28 +1,60 @@
-import unittest
 from csv_parser import StoresParser
+import unittest
 
 
 class TestStoresParser(unittest.TestCase):
+    """Test StoresParser functionality.
+
+    """
+
     def setUp(self):
+        """Initialize StoresParser instance with store-locations.csv.
+
+        """
+
         self.sp = StoresParser('store-locations.csv')
 
     def test_file_path(self):
+        """Test that file_path getter returns correct file_path.
+
+        """
+
         self.assertEqual(self.sp.file_path, 'store-locations.csv')
 
     def test_encoding(self):
+        """Test that encoding getter returns correct encoding.
+
+        """
+
         self.assertEqual(self.sp.encoding, 'utf-8-sig')
 
     def test_delimiter(self):
+        """Test that delimiter getter returns correct delimiter.
+
+        """
+
         self.assertEqual(self.sp.delimiter, ',')
 
     def test_dict_initial(self):
+        """Test that dict getter returns None initially before get_dict has run.
+
+        """
+
         self.assertEqual(self.sp.dict, None)
 
     def test_get_dict(self):
+        """Test that get_dict parses the csv into a list of 1791 stores.
+
+        """
+
         dict = self.sp.get_dict()
         self.assertEqual(len(dict), 1791)
 
     def test_dict_after_get_dict(self):
+        """Test that dict getter returns list of stores after get_dict has run.
+
+        """
+
         dict = self.sp.get_dict()
         self.assertEqual(self.sp.dict, dict)
 
