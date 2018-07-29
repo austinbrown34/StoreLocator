5870d04355f049d6f803ddb995a5251711db5129
Austin <austinbrown34@gmail.com>
2018-07-29 16:59:49 -0600

    feat: Add KDTree functionality and Pickling to StoresParser

--- csv_parser.py
+++ csv_parser.py
@@ -1,9 +1,18 @@
 import codecs
 from constants import (
     DEFAULT_DELIMITER,
-    DEFAULT_ENCODING
+    DEFAULT_ENCODING,
+    STORE_FIELDS
 )
 import csv
+import numpy
+import os
+import pickle
+from scipy.spatial import KDTree
+from util import (
+    euclidean_distance,
+    geodetic2ecef
+)
 
 
 class StoresParser(object):
@@ -33,7 +42,20 @@ class StoresParser(object):
         self.file_path = file_path
         self.encoding = encoding
         self.delimiter = delimiter
-        self.dict = None
+        self.stores = None
+        self.tree = None
+
+    @staticmethod
+    def get_StoresParser(stores_csv):
+        if os.path.exists('{}.pkl'.format(stores_csv)):
+            with open('{}.pkl'.format(stores_csv), 'rb') as input:
+                sp = pickle.load(input)
+        else:
+            sp = StoresParser(stores_csv)
+            sp.get_stores()
+            sp.build_tree()
+            sp.save()
+        return sp
 
     @property
     def file_path(self):
@@ -84,27 +106,91 @@ class StoresParser(object):
         self.__delimiter = delim
 
     @property
-    def dict(self):
-        """Getter for dict.
+    def stores(self):
+        """Getter for stores.
 
         """
 
-        return self.__dict
+        return self.__stores
 
-    @dict.setter
-    def dict(self, dic):
-        """Setter for dict.
+    @stores.setter
+    def stores(self, stores):
+        """Setter for stores.
 
         """
 
-        self.__dict = dic
+        self.__stores = stores
 
-    def get_dict(self):
+    @property
+    def tree(self):
+        """Getter for tree.
+
+        """
+
+        return self.__tree
+
+    @tree.setter
+    def tree(self, tree):
+        """Setter for tree.
+
+        """
+
+        self.__tree = tree
+
+    def save(self):
+        """Saves StoresParser instance to .pkl file.
+
+        """
+
+        with open('{}.pkl'.format(self.file_path), 'wb') as output:
+            pickle.dump(self, output, pickle.HIGHEST_PROTOCOL)
+
+    def build_tree(self):
+        """Creates KDTree representation of coordinate data.
+
+        """
+
+        if self.stores is not None:
+            stores_ecef = []
+            for store in self.stores:
+                store_ecef = geodetic2ecef(
+                    float(store[STORE_FIELDS['LATITUDE']]),
+                    float(store[STORE_FIELDS['LONGITUDE']])
+                )
+                store['ecef'] = store_ecef
+                stores_ecef.append(store_ecef)
+            self.tree = KDTree(numpy.array(stores_ecef))
+
+    def query(self, target_ecef, radius):
+        """Searches for stores within a given radius of a location.
+
+        Args:
+            target_ecef (float): XYZ ECEF coords to search against.
+            radius (float): Search radius.
+        Returns:
+            List of stores within a given radius of a location.
+
+        """
+
+        results = None
+        if self.tree is not None:
+            matches = self.tree.query_ball_point(
+                [target_ecef],
+                r=euclidean_distance(radius)
+            )
+            if len(matches):
+                results = [self.stores[match] for match in matches[0]]
+        return results
+
+    def get_stores(self):
         """Parses stores data from CSV and returns a list of stores (dicts).
+
+        The list of stores that is returned is sorted by latitude and then
+        longitude.
 
         """
 
         with codecs.open(self.file_path, 'r', encoding=self.encoding) as f:
             reader = csv.DictReader(f, delimiter=self.delimiter)
-            self.dict = list(reader)
-        return self.dict
+            self.stores = list(reader)
+        return self.stores
