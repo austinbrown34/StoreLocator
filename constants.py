cbb8e4906db03d5e9c293e82d734be32935f6865
austinbrown34 <austinbrown34@gmail.com>
2018-07-29 01:32:20 -0600

    Refactor cleanup (#6)
    * refactor: Move validate_args to util
    
    * fix: Add KILOMETERS_TO_MILES, DISTANCE_RADIUS, STORE_FIELDS to constants
    
    * refactor: Replace hardcoded values with constants refs

--- constants.py
+++ constants.py
@@ -6,3 +6,19 @@ DEFAULT_DELIMITER = ','
 UNITS = ['mi', 'km']
 OUTPUT = ['text', 'json']
 DISTANCE_PRECISION = 2
+KILOMETERS_TO_MILES = 0.621371
+DISTANCE_RADIUS = 6371
+
+# CSV Field Names
+STORE_FIELDS = {
+    'NAME': 'Store Name',
+    'LOCATION': 'Store Location',
+    'COUNTY': 'County',
+    'ADDRESS': 'Address',
+    'CITY': 'City',
+    'STATE': 'State',
+    'ZIP_CODE': 'Zip Code',
+    'LATITUDE': 'Latitude',
+    'LONGITUDE': 'Longitude',
+    'DISTANCE': 'Distance'
+}
